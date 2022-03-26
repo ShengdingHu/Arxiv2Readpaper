3fb3e2528287b324c2f0041a30275f7fe11e9253
shengdinghu <shengdinghu@gmail.com>
2022-03-26 14:43:31 +0800

    add pdf not parsed support

--- pdfparse.js
+++ pdfparse.js
@@ -1,8 +1,17 @@
 title = chrome.storage.sync.get(['title'], function(result) {
+    if (result.title.endsWith(".pdf")){
+        var titletext = prompt("Title not parsed, please copy it manually.")
+        if (titletext != null){
+            var choice = true
+        }else{
+            var choice = false
+        }
+    }else{
     var choice = confirm("Search \""+result.title+"\" on ReadPaper?");
+    titletext = result.title
+    }
 
 if (choice){
-    titletext = result.title
     console.log(titletext)
     search_content = `https://readpaper.com/search/${encodeURIComponent(titletext)}`
     console.log(search_content)
